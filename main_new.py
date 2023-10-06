9771945a9a6b6035d17941f4dd9f2a15234cb23d
sea <c.eres5519@gmail.com>
2023-10-06 15:11:45 +0100

    added score, and stopped old beats from persisting on the screen

--- main_new.py
+++ main_new.py
@@ -4,7 +4,7 @@ import pygame
 from pygame.locals import QUIT
 
 
-class Game_Manager:
+class GameManager:
     def __init__(self) -> None:
         # initialising pygame window
         pygame.init()
@@ -12,57 +12,64 @@ class Game_Manager:
         pygame.display.set_caption('Rythmn Game')
         self.clock = pygame.time.Clock()
         self.player = Player(self.window)
-        self.song = Song('pulsar.wav',False)
+        self.song = Song('pulsar.wav', False)
         pygame.mixer.init()
 
         # initialising beats, each beats default colour is slightly different
         # from those adjacent to it
-        self.beats = [Beat(self.window, x=i*90, colour_shift=abs((i-4))*10, active=False) for i in range(8)]
+        self.beats = [Beat(self.window, x=i * 90, colour_shift=abs((i - 4)) * 10, active=False) for i in range(8)]
 
         self.__active_beat = self.beats[self.song.sequence[self.song.current_note]]
         self.__active_range = self.__active_beat.set_active()
         self.speed = 6
-        
+
     def game_loop(self):
         while True:
-            self.window.fill((255,255,255))
+            self.window.fill((255, 255, 255))
             for beat in self.beats:
                 beat.draw_self()
             self.player.draw_self(self.speed)
+            self.player.score.update_score()
 
             if not self.song.playing:
                 self.song.play()
                 self.song.playing = True
-          
+
             space_pressed = False
             keys = pygame.key.get_pressed()
             if keys[pygame.K_SPACE] and not space_pressed:
                 space_pressed = True
-                #self.player.disallow_direction_change()
+                # self.player.disallow_direction_change()
             else:
                 # only allows direction to be changed after the space bar is depressed
-                #self.player.allow_direction_change()
+                # self.player.allow_direction_change()
                 space_pressed = False
 
             match self.player_in_beat():
                 case 0:
                     if space_pressed:
                         self.__active_beat.set_hit((252, 73, 73))
+                        self.player.score.update_score("none")
                 case 1:
                     if space_pressed:
                         self.__active_beat.set_hit((73, 252, 73))
-                        self.player.score += 5
+                        self.player.score.update_score("good")
                 case 2:
                     if space_pressed:
                         self.__active_beat.set_hit((73, 252, 252))
-                        self.player.score += 10
+                        self.player.score.update_score("perfect")
                 case 3:
                     self.__active_beat.set_hit((252, 73, 73))
+                    self.player.score.update_score("none")
                     self.player.change_direction()
                     self.player.allow_direction_change()
+                    self.__active_beat.set_inactive()
+
+                    # selecting the next active note
                     self.song.current_note += 1
                     self.__active_beat = self.beats[self.song.sequence[self.song.current_note]]
                     self.__active_range = self.__active_beat.set_active()
+                    self.player.score.unlock_score_update()
 
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -78,18 +85,18 @@ class Game_Manager:
         direction = self.player.get_direction()
         # 2: perfect score, 1: partial score, 0: no score, 3: player overshoot
         if direction == "right":
-            if player_range[1]+self.speed >= self.__active_range[1]:
+            if player_range[1] + self.speed >= self.__active_range[1]:
                 return 3
-            elif player_range[1]+self.speed >= self.__active_range[0]:
-                if player_range[0]+self.speed >= self.__active_range[0]:
+            elif player_range[1] + self.speed >= self.__active_range[0]:
+                if player_range[0] + self.speed >= self.__active_range[0]:
                     return 2
                 return 1
             return 0
         elif direction == "left":
-            if player_range[0]-self.speed <= self.__active_range[0]:
+            if player_range[0] - self.speed <= self.__active_range[0]:
                 return 3
-            elif player_range[0]-self.speed <= self.__active_range[1]:
-                if player_range[1]-self.speed <= self.__active_range[1]:
+            elif player_range[0] - self.speed <= self.__active_range[1]:
+                if player_range[1] - self.speed <= self.__active_range[1]:
                     return 2
                 return 1
             return 0
@@ -109,21 +116,22 @@ class Rectangle:
         pygame.draw.rect(self._window, self._colour, rect)
         if isPlayer:
             # the player appears to move seamlessly from one side of the screen to the other
-            rect1 = pygame.Rect(self._x+720,self._y,self._width,self._height)
-            rect2 = pygame.Rect(self._x-720,self._y,self._width,self._height)
-            pygame.draw.rect(self._window,self._colour,rect1)
-            pygame.draw.rect(self._window,self._colour,rect2)
-        
+            rect1 = pygame.Rect(self._x + 720, self._y, self._width, self._height)
+            rect2 = pygame.Rect(self._x - 720, self._y, self._width, self._height)
+            pygame.draw.rect(self._window, self._colour, rect1)
+            pygame.draw.rect(self._window, self._colour, rect2)
+
     def get_range(self):
-        return self._x, self._x+self._width
+        return self._x, self._x + self._width
 
 
 class Player(Rectangle):
     def __init__(self, window) -> None:
-        super().__init__(window, window.get_width()//2, window.get_height()//2 - 25, 90, 90, (146, 99, 247))
+        super().__init__(window, window.get_width() // 2, window.get_height() // 2 - 25, 90, 90, (146, 99, 247))
         self.__direction = "right"
         self.__direction_change = True
-        self.score = 0
+        self.score = Score(window)
+        self.score.update_score()
 
     def draw_self(self, delta_x):
         self._x += self.__calculate_move(delta_x)
@@ -144,7 +152,7 @@ class Player(Rectangle):
     def disallow_direction_change(self):
         if self.__direction_change:
             self.__direction_change = False
-        
+
     def allow_direction_change(self):
         if not self.__direction_change:
             self.__direction_change = True
@@ -173,53 +181,84 @@ class Beat(Rectangle):
         if self._active:
             self._active = False
             self._colour = colour
-    
+
     def set_inactive(self):
         self._active = False
         self._colour = self._default_colour
+
+class Score:
+    def __init__(self, window):
+        self.__window = window
+        # monospace is the best font dont @ me :3
+        self.__font = pygame.font.SysFont("monospace", 50)
+        self.__score = 0
+        self.__allow_update = True
+
+    def __write_score(self):
+        # score text has to be rendered every time it is updated
+        score_surface = self.__font.render(f"{self.__score}", True, (146, 99, 247))
+        self.__window.blit(score_surface, (0,0))
+
+    def __increase_score(self, beat_success):
+        # score can only update once per beat
+        if self.__allow_update and beat_success:
+            match beat_success:
+                case "perfect":
+                    self.__score += 10
+                case "good":
+                    self.__score += 5
+                case "none":
+                    pass
+            self.__allow_update = False
+
+    def unlock_score_update(self):
+        self.__allow_update = True
+
+    def update_score(self, beat_success=""):
+        self.__increase_score(beat_success)
+        self.__write_score()
 
 
 class Song:
     def __init__(self, source, playing):
         self.source = source
-        self.sequence = [ #      Song sequence in note length form
-            2,2,2,2,2,2,2,2,
-            2,2,2,2,2,2,2,2,
-            2,2,2,2,1,1,1,2,1,1,1,
-            2,2,2,1,1,1,1,1,1,1,1,1,1,
-            2,2,2,2,1,1,1,2,1,1,1,
-            2,2,2,1,1,1,1,1,1,1,1,1,1,
-            2,2,2,2,1,1,1,2,1,1,1,
-            2,2,2,1,1,1,1,1,1,1,1,1,1,
-            2,2,2,2,1,1,1,2,1,1,1,
-            2,2,2,1,1,1,1,1,1,1,1,1,1,
-            2,2,2,2,1,1,1,2,1,1,1,
-            2,2,2,1,1,1,1,1,1,1,1,1,1,
-            2,2,2,2,1,1,1,2,1,1,1,
-            2,2,2,1,1,1,1,1,1,1,1,1,1,
-            2,2,2,2,1,1,1,2,1,1,1,
-            2,2,2,1,1,1,1,1,1,1,1,1,1,
-            ]
+        self.sequence = [  # Song sequence in note length form
+            2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1,
+            2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1,
+            2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1,
+            2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1,
+            2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1,
+            2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1,
+            2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1,
+            2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ]
         self.playing = playing
         self.current_note = 0
         # Calculate which beats correspond to the note lengths above
         new_sequence = [4]
 
         for i in range(len(self.sequence)):
-            if i%2 == 0:
-                new_sequence.append((new_sequence[i]-self.sequence[i])%8)
+            if i % 2 == 0:
+                new_sequence.append((new_sequence[i] - self.sequence[i]) % 8)
             else:
-                new_sequence.append((new_sequence[i]+self.sequence[i])%8)
-        
+                new_sequence.append((new_sequence[i] + self.sequence[i]) % 8)
+
         self.sequence = new_sequence
 
     def play(self):
         pygame.mixer.music.load('pulsar.wav')
         pygame.mixer.music.set_volume(1)
         pygame.mixer.music.play()
-        
-        
+
 
 if __name__ == '__main__':
-    game_manager = Game_Manager()
+    game_manager = GameManager()
     game_manager.game_loop()
